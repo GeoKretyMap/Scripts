7384a3b16071110e389c29394432e9d462b6fc77
Mathieu Alorent <github@kumy.net>
2015-05-03 23:48:04 +0200

    remove minimal value

--- rrd/create_rrd.sh
+++ rrd/create_rrd.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#http://www.famzah.net/rrdwizard/rrdcreate.php?
 
-rrdtool create filename.rrd --step '900' 'DS:BASIC:GAUGE:1350:0:U' 'DS:DETAILS:GAUGE:9000:0:U' 'RRA:AVERAGE:0.5:1:900' 'RRA:AVERAGE:0.5:10:300' 'RRA:AVERAGE:0.5:15:600' 'RRA:AVERAGE:0.5:50:1000' 'RRA:AVERAGE:0.5:200:4380'
+rrdtool create filename.rrd --step '900' 'DS:BASIC:GAUGE:1350:U:U' 'DS:DETAILS:GAUGE:9000:U:U' 'RRA:AVERAGE:0.5:1:900' 'RRA:AVERAGE:0.5:10:300' 'RRA:AVERAGE:0.5:15:600' 'RRA:AVERAGE:0.5:50:1000' 'RRA:AVERAGE:0.5:200:4380'

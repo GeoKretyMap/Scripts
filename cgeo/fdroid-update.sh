65a4a250e5939ea92e198faa6cb80751d3e6e841
Mathieu Alorent <github@kumy.net>
2015-05-18 22:08:51 +0200

    Rework script

--- cgeo/fdroid-update.sh
+++ cgeo/fdroid-update.sh
@@ -2,8 +2,7 @@
 
 fdroid_dir="/home/fdroid/www"
 
-apk_mainline="http://www.cgeo.org/cgeo.apk"
-apk_nightly="http://www.cgeo.org/c-geo-nightly.apk"
+apk_nightly="http://download.cgeo.org"
 
 aapt="/opt/android-sdk/build-tools/22.0.1/aapt"
 fdroid="/opt/fdroidserver/fdroid"
@@ -15,32 +14,49 @@ function usage {
    exit 1
 }
 
+# Download apk
+function download_apk {
+   apk=$1
+   url=$2
+   release=$3
+
+   cd $fdroid_dir/$release/repo
+   wget -q -O tmp-${apk}.apk $url || { echo "Fail to download apk"; exit 2; }
+   apk_version=`$aapt dump badging tmp-${apk}.apk | head -n1 | sed "s/.*versionName='\([^ ]*\)' .*/\1/"`
+   
+   [[ $apk_version =~ ^[legacyNOJITBa-f0-9.-]+$ ]] || { echo "Fail to extract apk version"; exit 3; }
+   
+   mv tmp-${apk}.apk ${apk}-${apk_version}.apk
+}
+
+# Update indexes
+function update_indexes {
+   release=$1
+
+   cd $fdroid_dir/$release; $fdroid update -q
+   
+   exit $?
+}
+
+
 if [[ $1 != "nightly" && $1 != "mainline" ]]; then
    usage
 fi
 
 if [[ $1 == "mainline" ]]; then
-   apk="cgeo"
-   url=$apk_mainline
-   dir="mainline"
+   for apk in $(curl -s https://api.github.com/repos/cgeo/cgeo/releases/latest | grep 'browser_' | cut -d\" -f4); do
+      apk_name=$(echo ${apk##*/} | cut -d '_' -f 1)
+      download_apk "${apk_name}" "$apk" "mainline"
+   done
+   update_indexes "mainline"
 fi
 
 if [[ $1 == "nightly" ]]; then
-   apk="c-geo-nightly"
-   url=$apk_nightly
-   dir="nightly"
+   # - cgeo-nightly-nojit lead to duplicate versions
+   # - Contact is not yet available
+   #for apk in cgeo-nightly cgeo-nightly-nojit cgeo-calendar-nightly cgeo-contacts-nightly; do
+   for apk in cgeo-nightly cgeo-calendar-nightly; do
+      download_apk "${apk}" "${apk_nightly}/${apk}.apk" "nightly"
+   done
+   update_indexes "nightly"
 fi
-
-# obtain latest apk
-cd $fdroid_dir/$dir/repo
-wget -q -O tmp-${apk}.apk $url || { echo "Fail to download apk"; exit 2; }
-apk_version=`$aapt dump badging tmp-${apk}.apk | head -n1 | sed "s/.*versionName='\([^ ]*\)' .*/\1/"`
-
-[[ $apk_version =~ ^[NBa-f0-9.-]+$ ]] || { echo "Fail to extract apk version"; exit 3; }
-
-mv tmp-${apk}.apk ${apk}-${apk_version}.apk
-
-# update indexes
-cd $fdroid_dir/$dir; $fdroid update -q
-
-exit $?
